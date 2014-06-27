dd5806c001fac8c485b1be8a41b87e0d7486ec22
xiaohui.ma <xiaohui.ma@t2mobile.com>
2014-06-27 15:15:31 +0800

    Revert "mozilla bug 1013734"
    This reverts commit df4e459881abf6c1909dda5d92d9d9ac12d4d9bc.

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -237,7 +237,7 @@ unsigned char *update_cmdline(const char * cmdline)
 		cmdline_len += strlen(ffbm_mode_string);
 		/* reduce kernel console messages to speed-up boot */
 		cmdline_len += strlen(loglevel);
-	} else if (device.charger_screen_enabled && target_pause_for_battery_charge()) {
+	} else if (target_pause_for_battery_charge()) {
 		pause_at_bootup = 1;
 		cmdline_len += strlen(battchg_pause);
 	}
